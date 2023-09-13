2e98c7b9180bceeab43df8723b281b3bf0d00840
SalsabelDalaq <95722891+SalsabelDalaq@users.noreply.github.com>
2023-09-12 20:15:37 -0500

    Update myfunctions.h

--- SalsabeelDalaqCS-303-Assignment-1/myfunctions.h
+++ SalsabeelDalaqCS-303-Assignment-1/myfunctions.h
@@ -1,4 +1,4 @@
-// myfunctions.h
+
 
 #ifndef MYFUNCTIONS_H
 #define MYFUNCTIONS_H
@@ -9,4 +9,4 @@ void modify(int arr[], int index, int &num, int &old);
 void add(int arr[], int &size, int num);
 void remove(int arr[], int &size, int index);
 
-#endif // MYFUNCTIONS_H
+#endif 
